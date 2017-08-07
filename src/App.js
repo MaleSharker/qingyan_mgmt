2cfc0f818b5fd6d5adcd1e24e5ff6f1d12bed678
YCYL <YCYL@UIMac1.local>
2017-08-07 20:34:08 +0800

    iupdate

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render(){
+		console.log('something happened');
+		return (
+			<div>This is a react boileraplate</div>
+			);
+	}
+}
